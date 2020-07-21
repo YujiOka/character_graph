5e63186c59f88ef3cf0f49371ef54863ac4b6263
YujiOka <s16t215@stu.kagawa-u.ac.jp>
2020-07-22 03:51:21 +0900

    関係表現の宛先決定方法変更、二次元配列のindex検索用メソッド追加

--- plot_chara_graph.py
+++ plot_chara_graph.py
@@ -21,12 +21,38 @@ def connect_check(c_1, c_2, connect):
             return con[2]["weight"]
     return 0
 
+# 関係表現からの距離を算出
+def calc_distance(pos, distance, start_dis, end_dis):
+    for i in range(len(distance)):
+        if end_dis < distance[i]:
+            distance[i] = (distance[i] - pos) * 1.5
+        elif start_dis > distance[i]:
+            distance[i] = (pos - distance[i]) * 1.5
+        elif distance[i] > pos:
+            distance[i] = distance[i] - pos
+        else:
+            distance[i] = pos - distance[i]
+
+# 関係表現の宛先を決定
+def calc_rel(pos, candidate, distance, start_dis, end_dis):
+    tmp1 = candidate[0]
+    tmp2 = candidate[1]
+    calc_distance(pos, distance, start_dis, end_dis)
+    for i in range(2, len(distance)):
+        if distance[candidate.index(tmp2)] > distance[i]:
+            tmp2 = candidate[i]
+    for j in range(1, len(distance)):
+        if distance[candidate.index(tmp1)] > distance[j] and tmp2 != candidate[j]:
+            tmp1 = candidate[j]
+    return tmp1, tmp2
+
 # 関係表現を全て抽出
 def get_relation_name(text, rel_position):
     num = rel_position + 1
     name = text[rel_position][0]
     while text[num][1][-2].replace("B-", "").replace("I-", "") == "REL":
         name += text[num][0]
+        num += 1
     return name
 
 # 関係表現からエッジのラベルを命名
@@ -36,19 +62,37 @@ def detect_relation(text, rel_position):
         return "家族"
     return get_relation_name(text, rel_position)
 
+# 関係表現周辺の人物とその位置を抽出
+def search_distance(text, pos, candidate, distance, after_flag, start_dis, end_dis):
+    for i in range(len(text)):
+        if i < pos and text[i][0] == "。":
+            start_dis = i
+        if i > pos:
+            if text[i][0] == "。":
+                end_dis = i
+            after_flag = True
+        if text[i][1][-1] != "O":
+            candidate.append(text[i][1][-1])
+            distance.append(i)
+            if after_flag and text[i][0] == "。":
+                end_dis = i
+                break
+    return start_dis, end_dis
+
 # 関係表現に最も近い二人のキャラのエッジにラベル付け
 def add_rel_to_edge(G, text, rel_position):
-    before_candidate = []  # 関係表現より前のキャラ
-    after_candidate = []  # 関係表現より後のキャラ
-    for posi in rel_position:
-        for i in range(posi):
-            if text[i][1][-1] != "O":
-                before_candidate.append(text[i][1][-1])
-        for i in range(posi+1, len(text)):
-            if text[i][1][-1] != "O":
-                after_candidate.append(text[i][1][-1])
-        rel = detect_relation(text, posi)
-        G.add_edge(before_candidate[-1], after_candidate[0], label=rel)
+    # before_candidate = []  # 関係表現より前のキャラ
+    # after_candidate = []  # 関係表現より後のキャラ
+    candidate = [] # 関係表現の宛先候補
+    distance = [] # 関係表現からの距離
+    after_flag = False
+    start_dis = -1
+    end_dis = -1
+    for pos in rel_position:
+        start_dis, end_dis = search_distance(text, pos, candidate, distance, after_flag, start_dis, end_dis)
+        rel = detect_relation(text, pos)
+        can_1, can_2 = calc_rel(pos, candidate, distance, start_dis, end_dis)
+        G.add_edge(can_1, can_2, label=rel)
     
 
 # 文章に出てきたキャラを関連付け
@@ -58,7 +102,7 @@ def connect_chara(t_chara, G, rel_position, text):
         for j in range(i+1, len(t_chara)):
             w_num = connect_check(t_chara[i], t_chara[j], e_cnct)
             if w_num == 0:
-                G.add_edge(t_chara[i], t_chara[j], weight=1, label="nomal")
+                G.add_edge(t_chara[i], t_chara[j], weight=1, label="")
             else:
                 G.add_edge(t_chara[i], t_chara[j], weight=w_num+1)
     if rel_position[0] != -1:
@@ -72,12 +116,19 @@ def remember_relation(rel_position, text, t):
     else:
         rel_position.append(text.index(t))
 
+# 二次元配列charaのインデックスを検索
+def search_chara(chara, text):
+    for i in range(len(chara)):
+        if chara[i] == text:
+            break
+    return i
+
 # 性別によってノードの色を変化
 def calc_sex(text, chara):
     if text[0] in ["彼"]:
-        chara[chara[0].index(text[1][-1])][2] = "blue"
+        chara[search_chara(chara, text[1][-1])][2] = "blue"
     elif text[0] in ["彼女"]:
-        chara[chara[0].index(text[1][-1])][2] = "red"
+        chara[search_chara(chara, text[1][-1])][2] = "red"
 
 # キャラを計数
 def calc_chara(chara, t_chara, text):
@@ -85,7 +136,7 @@ def calc_chara(chara, t_chara, text):
     if text[1][-1] not in name:
         chara.append([text[1][-1], 1, "green"])  # 名前、weight、名前の色（性別）
     else:
-        chara[chara[0].index(text[1][-1])][1] += 1
+        chara[search_chara(chara, text[1][-1])][1] += 1
     if text[1][-2].replace("B-","") != "NAME":
         calc_sex(text, chara)
     if text[1][-1] not in t_chara:
@@ -104,6 +155,7 @@ def calc_sent(G, text):
     rel_position = [-1]  # 関係表現の出現位置
     for t in text:
         if t[0] == "。":
+            print(t_chara)
             end_sent(G, t_chara, rel_position, text)
         elif t[1][-1] != "O":
             calc_chara(chara, t_chara, t)
@@ -145,10 +197,10 @@ def make_graph():
     # colors = ["red","green","yellow","blue","magenta"]
     # node_sizes = [1200,1200,1200,1200,1200]
     # edge_labels = {("明治", "大正"):"親子", ("大正", "昭和"):"師弟", ("昭和", "平成"):"仲間", ("平成", "令和"):"家族", ("昭和", "明治"):"メンバー"}
-    print(G.edges(data=True))
+    # print(G.edges(data=True))
     # labels = {("ゴーシュ", "アレン"):"仕事仲間"}
     # print(labels)
-    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('Reds'), node_color=[j["color"] for i, j in G.nodes(data=True)] ,alpha=0.5, node_size=[j["weight"]*100 for i, j in G.nodes(data=True)])
+    nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('Reds'), node_color=[j["color"] for i, j in G.nodes(data=True)] ,alpha=0.5, node_size=[j["weight"]*1500 for i, j in G.nodes(data=True)])
     # nx.draw_networkx_edges(G, pos, edge_color="black", edgelist=black_edges, width=[k["weight"] for i, j, k in G.edges(data=True)])
     nx.draw_networkx_edges(G, pos, edge_color="black", width=[k["weight"] for i, j, k in G.edges(data=True)])
     nx.draw_networkx_labels(G,pos,font_family='IPAexGothic')
